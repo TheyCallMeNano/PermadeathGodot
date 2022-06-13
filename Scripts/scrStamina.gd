f87e2cf7c71129d5498398ff54205a44a1e17df1
TheyCallMeNano <xnanoxplaysmc@gmail.com>
2022-06-12 23:05:59 -0500

    Built-In Lighting System with Tileset Collisions

--- Scripts/scrStamina.gd
+++ Scripts/scrStamina.gd
@@ -6,7 +6,7 @@ func _process(delta):
 		add_color_override("font_color", Color(1,1,1,1))
 		set_text("Stamina: " + str(global.plrStamina))
 	#Print text as red when stamina is empty and sprinting/dashing
-	elif global.plrStamina == 0 && Input.is_action_pressed("sprint") || global.plrStamina == 0 && Input.is_action_pressed("dash"):
+	elif global.plrStamina == 0 && Input.is_action_pressed("sprint") || global.plrStamina < global.plrStaminaRecharge*30 && Input.is_action_pressed("dash"):
 		add_color_override("font_color", Color(255,0,0,1))
 		set_text("Stamina: " + str(global.plrStamina))
 	#Reset to normal when not active
